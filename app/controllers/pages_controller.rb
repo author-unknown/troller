74256b8e9d67a3990fa2a1bd7c6daf38f51f581b
Zach <z.little415@gmail.com>
2017-08-02 19:21:05 +0000

    Created pages. Override of default routes.

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,22 @@
+#The pages controller contains code for all pages inside /pages
+
+class PagesController < ApplicationController
+  
+  #back-end code for pages/index
+  def index
+  end
+
+  #back-end code for pages/home
+  def home
+  end
+  
+  
+  #back-end code for pages/profile
+  def profile
+  end
+
+
+  #back-end code for pages/explore
+  def explore
+  end
+end
